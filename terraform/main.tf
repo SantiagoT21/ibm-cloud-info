a8db0dfb8719080174f208371df67b694413eaf5
SantiagoT21 <stamayol@unal.edu.co>
2026-05-05 11:55:43 -0500

    Fix: Remove unsupported arguments from ibm_code_engine_project resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,12 +7,4 @@ data "ibm_resource_group" "resource_group" {
 resource "ibm_code_engine_project" "project" {
   name              = var.project_name
   resource_group_id = data.ibm_resource_group.resource_group.id
-  
-  tags = var.tags
-
-  timeouts {
-    create = "15m"
-    update = "15m"
-    delete = "15m"
-  }
 }
