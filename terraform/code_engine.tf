237c3853d57cb4f55c8e455904f95ccd3b309e3f
SantiagoT21 <stamayol@unal.edu.co>
2026-05-05 12:28:06 -0500

    Fix: Simplify app configuration to minimal required parameters

--- terraform/code_engine.tf
+++ terraform/code_engine.tf
@@ -6,16 +6,9 @@ resource "ibm_code_engine_app" "app" {
   image_reference = var.container_image
   image_port      = var.container_port
   
-  # Scaling configuration
-  scale_min_instances   = var.min_scale
-  scale_max_instances   = var.max_scale
-  scale_cpu_limit       = var.cpu
-  scale_memory_limit    = var.memory
-  scale_request_timeout = var.request_timeout
-  
-  # Concurrency
-  scale_concurrency        = 100
-  scale_concurrency_target = 80
+  # Minimal scaling configuration
+  scale_cpu_limit    = var.cpu
+  scale_memory_limit = var.memory
 
   depends_on = [
     ibm_code_engine_project.project
