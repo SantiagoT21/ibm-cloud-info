2582fde014a7eb6e96eb2600460ff6eda91a99f8
SantiagoT21 <stamayol@unal.edu.co>
2026-05-06 10:35:54 -0500

    Fix: Use new project ibm-cloud-info-v4 to avoid soft-deleted project issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,12 +3,8 @@ data "ibm_resource_group" "resource_group" {
   name = var.resource_group_name
 }
 
-# Create Code Engine project (or use existing)
+# Create Code Engine project
 resource "ibm_code_engine_project" "project" {
   name              = var.project_name
   resource_group_id = data.ibm_resource_group.resource_group.id
-  
-  lifecycle {
-    ignore_changes = all
-  }
 }
